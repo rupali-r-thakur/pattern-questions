f68d8b28c2db3acd27500d7180ab3af8b0a82c63
rupali-r-thakur <rupali97097@gmail.com>
2023-03-12 23:11:33 +0530

    solve-pattern-question

--- solvePatternQuestions.c
+++ solvePatternQuestions.c
@@ -54,16 +54,34 @@
 //  ****
 // *****
 
-#include<stdio.h>
-void main (){
-  int row=5;
-  for (int i=0; i<row; i++){
-    for(int s=0; s<=row-i+1; s++){
-      printf(" ");
-    }
-    for(int j=0; j<i+1; j++){
-      printf("*");
-    }
-    printf("\n");
-  }
-}
+// #include<stdio.h>
+// void main (){
+//   int row=5;
+//   for (int i=0; i<row; i++){
+//     for(int s=0; s<=row-i+1; s++){
+//       printf(" ");
+//     }
+//     for(int j=0; j<i+1; j++){
+//       printf("*");
+//     }
+//     printf("\n");
+//   }
+// }
+
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+ #include<stdio.h>
+        void main (){
+            int num=5;
+            for(int n=0; n<num; n++){
+                for(int number=0;number<num-n; number++){
+                    printf(" %d", number+1);
+                }
+                printf("\n");
+            }
+        }
