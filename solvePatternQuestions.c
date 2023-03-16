743601d9dcb06a635857258730bfdf65f026cdef
rupali-r-thakur <rupali97097@gmail.com>
2023-03-16 22:53:27 +0530

    solve-pattern-question

--- solvePatternQuestions.c
+++ solvePatternQuestions.c
@@ -147,44 +147,61 @@
 //             *
 
     
+    // #include<stdio.h>
+    // void main (){
+    //     int row=5;
+    //     for (int i=0; i<row; i++){
+    //         if(i==0){
+    //             printf(" ");
+    //         }
+    //         for(int n=0; n<row-i; n++){
+    //             printf("  ");
+    //         }
+    //         printf(" *");
+    //          for(int j=0;j<2*i;j++){
+    //             printf( "  ");
+    //         }
+    //         if(i>0)
+    //         printf( " *");
+
+    //         printf("\n");
+    //     }
+
+    //     for (int i=row; i>=0; i--){
+    //         if(i==0){
+    //             printf(" ");
+
+    //         }
+    //         for(int n=0; n<row-i; n++){
+    //             printf("  ");
+            
+    //         }
+            
+    //         printf(" *");
+    //          for(int j=0;j<2*i;j++){
+    //             printf("  ");
+    //         }
+    //         if(i>0)
+    //         printf(" *");
+
+    //         printf("\n");
+    //     }
+        
+    // }
+
+    // 1
+    // 1 2
+    // 1 2 3
+    // 1 2 3 4
+    // 1 2 3 4 5
+
     #include<stdio.h>
     void main (){
-        int row=5;
-        for (int i=0; i<row; i++){
-            if(i==0){
-                printf(" ");
+        int row=10;
+        for(int i=1; i<=row; i++){
+            for (int n=1; n<=i; n++){
+                printf(" %d", n);
             }
-            for(int n=0; n<row-i; n++){
-                printf("  ");
-            }
-            printf(" *");
-             for(int j=0;j<2*i;j++){
-                printf( "  ");
-            }
-            if(i>0)
-            printf( " *");
-
-            printf("\n");
+            printf("\n" );
         }
-
-        for (int i=row; i>=0; i--){
-            if(i==0){
-                printf(" ");
-
-            }
-            for(int n=0; n<row-i; n++){
-                printf("  ");
-            
-            }
-            
-            printf(" *");
-             for(int j=0;j<2*i;j++){
-                printf("  ");
-            }
-            if(i>0)
-            printf(" *");
-
-            printf("\n");
-        }
-        
     }
